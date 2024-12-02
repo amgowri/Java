0b8b1a06804a654e1bb5473f597a1357ad8aa01a
Gowri <Gowri@192.168.1.10>
2024-12-02 17:16:15 +0530

    created java primitive data types

---
+++ Java/src/java_datatypes/JavaPrimitiveDatatypes.java
@@ -0,0 +1,30 @@
+package java_datatypes;
+
+public class JavaPrimitiveDatatypes {
+
+	public static void main(String[] args) {
+
+		byte myHouseNumber = 108;
+		short myAge = 29;
+
+		int myCGPA = 72;
+		long myPhoneNumber = 987653210L;
+
+		float myMonthlySalary = 80000.8F;
+		double myAnnualSalary = 960000.880;
+
+		boolean martialStatus = false;
+		char myGender = 'f';
+
+		System.out.println(" My Home Number is : " + myHouseNumber);
+		System.out.println(" My Age is : " + myAge);
+		System.out.println(" My CGPA : " + myCGPA);
+		System.out.println(" My Phone Number is : " + myPhoneNumber);
+		System.out.println(" My Monthly Salary is : " + myMonthlySalary);
+		System.out.println(" My Annual Salary is : " + myAnnualSalary);
+		System.out.println(" My Martial Status is : " + martialStatus);
+		System.out.println(" My Gender is : " + myGender);
+
+	}
+
+}
