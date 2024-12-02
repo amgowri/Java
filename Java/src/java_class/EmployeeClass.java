d47e1a249949c7c0fa742e223a19d908b2bb4b7d
Gowri <Gowri@192.168.1.10>
2024-12-02 14:36:28 +0530

    created employee class

---
+++ Java/src/java_class/EmployeeClass.java
@@ -0,0 +1,22 @@
+package java_class;
+
+public class EmployeeClass {
+
+	// Declare and initialize variables
+	String employeeName;
+	int employeeID;
+	double employeeSalary;
+
+	// method
+	void displayEmployeeInformation() {
+
+		// Print the value of the variable
+		System.out.println("Employee Name: " + employeeName + " " + "Employee ID: " + employeeID + " "
+				+ " Employee Salary: " + employeeSalary);
+	}
+
+	void setEmployeeInformation() {
+
+	}
+
+}
