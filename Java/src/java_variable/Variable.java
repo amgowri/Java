dd925bc5b00544b59a27079144fbdc3451cbc899
Gowri <Gowri@192.168.1.10>
2024-12-02 14:27:51 +0530

    created variable

---
+++ Java/src/java_variable/Variable.java
@@ -0,0 +1,14 @@
+package java_variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+
+		// Declare and initialize variable
+		int myAge = 29;
+
+		// Print the value of the variable
+		System.out.println(myAge);
+	}
+
+}
