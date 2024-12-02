598c88266f8ed1ca18d7c176ebd1553d2c604391
Gowri <Gowri@192.168.1.10>
2024-12-02 14:28:24 +0530

    created local variable

---
+++ Java/src/java_variable/LocalVariable.java
@@ -0,0 +1,18 @@
+package java_variable;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+
+		// Declare local variable
+		int myAge;
+
+		// Initialize local variable
+		myAge = 29;
+
+		// Print the value of the variable
+		System.out.println(myAge);
+
+	}
+
+}
