a4facd89653d04233a633a9d9d48de9093546836
Gowri <Gowri@192.168.1.10>
2024-12-06 18:39:12 +0530

    created employee main class

--- Java/src/java_class/EmployeeMainClass.java
+++ Java/src/java_class/EmployeeMainClass.java
@@ -1,16 +1,36 @@
 package java_class;
 
+/**
+ * 
+ * Object Syntax
+ * 
+ * Classname objectname = new Classname();
+ * 
+ * Object is an instance of class
+ * 
+ * Object is created by using new keyword followed by classname
+ * 
+ * Object doesn't occupy space in memory
+ * 
+ * Object is physical entity ( employee1, employee2 )
+ * 
+ * Using single class ( EmployeeClass ) we can have multiple objects( employee1,
+ * employee2, employee3 )
+ * 
+ */
 public class EmployeeMainClass {
 
 	public static void main(String[] args) {
 
 		// Employee 1
 		EmployeeClass employee1 = new EmployeeClass();
+		// Initialize variables
 		employee1.employeeName = "Gowri";
 		employee1.employeeID = 1001;
 		employee1.employeeSalary = 808040.84d;
 
 		employee1.displayEmployeeInformation();
+		// System.out.println(employee1.employeeName);
 
 		// Employee 2
 		EmployeeClass employee2 = new EmployeeClass();
